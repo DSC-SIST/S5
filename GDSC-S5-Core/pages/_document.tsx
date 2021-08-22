9c1e5a383f7127d06d3f13c129bc0f3276362d3e
harshcasper <hmishra@redhat.com>
2021-08-22 21:00:39 +0530

    feat: add GDSC S5 Website

---
+++ GDSC-S5-Core/pages/_document.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { IconContext } from 'react-icons'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
+        <Html lang='en'>
+          <Head>
+            <link rel='shortcut icon' href='/favicon.png' />
+          </Head>
+
+          <body>
+            <script src='noflash.js' />
+
+            <Main />
+
+            <NextScript />
+          </body>
+        </Html>
+      </IconContext.Provider>
+    )
+  }
+}
